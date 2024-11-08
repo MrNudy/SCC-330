47ec9bcb7d8833616cabc73560ebef2e4691f2fd
Aidan Tan <140573318+AidanTankj@users.noreply.github.com>
2024-11-08 00:34:12 +0000

    Delete database directory

--- database/compileserver.sh
+++
@@ -1,3 +0,0 @@
-g++ -o database_server database_server.cpp -lmysqlcppconn
-database_server
-
--- database/sqlsetup.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update 
-sudo apt install mysql-server
-sudo apt-get install libmysqlcppconn-dev
-sudo service mysql start
-sudo service mysql status
-sudo mysql_secure_installation
-
